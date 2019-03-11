97bf147c53b84d519bb24e2c588e34b66762e0fc
Tom Whitwell <tom@whi.tw>
2019-03-11 08:16:13 +0000

    More refactoring of nav bar
    Fixes #11
    
    Adds colours to navbar links

--- index.php
+++ index.php
@@ -1,29 +1,23 @@
 <html lang="en">
-  <?php /* Template Name: Default Template */ ?>
+  <?php /* Template Name: Front Page */ ?>
   <?php require 'template-parts/head/head.php';?>
   <body>
-    <div class="container-fluid">
+    <?php require 'template-parts/header/header.php';?>
+    <div class="content container">
       <div class="row">
-        <div class="col-md-12">
-          <?php require 'template-parts/header/header.php';?>
+        <div class="col-md">
+          <?php
+            if ( have_posts() ) : while ( have_posts() ) : the_post();
+              get_template_part( 'template-parts/content/content', 'single' );
+            endwhile;
+            else:
+              get_template_part( 'template-parts/content/content', 'single' );
+            endif;
+          ?>
         </div>
       </div>
       <div class="row">
-        <div id="page_container">
-          <div id="page_content">
-            <?php
-              if ( have_posts() ) : while ( have_posts() ) : the_post();
-                get_template_part( 'template-parts/content/content', 'single' );
-              endwhile;
-              else:
-                get_template_part( 'template-parts/content/content', 'single' );
-              endif;
-              ?>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12">
+        <div class="col-md">
           <?php get_footer(); ?>
         </div>
       </div>
