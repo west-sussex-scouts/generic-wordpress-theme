910786ea5b212a5e8b84f6b336d3bf79319a055e
Tom Whitwell <tom@whi.tw>
2019-03-11 08:16:13 +0000

    More refactoring of nav bar
    Fixes #11
    
    Adds colours to navbar links

--- front-page.php
+++ front-page.php
@@ -7,18 +7,18 @@
       <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.2"></script>
     <?php endif ?>
     <?php require 'template-parts/header/header.php';?>
-    <div class="content container">
+    <div class="content container pt-xl-4 pt-lg-4 pt-md-2 pt-sm-2 pt-1">
       <div class="row">
-        <div class="col-md">
+        <div class="col-lg">
           <h1><?php echo get_bloginfo( 'name', 'raw' )?></h1>
           <?= get_post_field('post_content', $post->ID) ?>
         </div>
-        <div class="col-3">
+        <div class="col-lg-4">
         <?php require 'template-parts/social_bar/social_bar.php';?>
         </div>
       </div>
       <div class="row">
-        <div class="col-md-12">
+        <div class="col-md">
           <?php get_footer(); ?>
         </div>
       </div>
--- template-parts/header/header.php
+++ template-parts/header/header.php
@@ -1,35 +1,29 @@
 <header id="home" class="header" itemtype="http://schema.org/WPHeader" style="min-height: 76px;">
-  <div id="main-nav" class="navbar" style="min-height: 76px;">
+  <nav class="navbar navbar-expand-md navbar-light" id="main-nav"  style="min-height: 76px;">
     <div class="container">
-      <div class="navbar-header responsive-logo">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navbar-brand">
-            <a href="/">
-              <?php
-                $custom_logo_id = get_theme_mod( 'custom_logo' );
-                $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
-              ?>
-              <img src="<?php echo $image[0]; ?>" alt="<?php echo get_bloginfo( 'name', 'raw' )?>"">
-            </a>
-        </div> <!-- /.navbar-brand -->
-      </div> <!-- /.navbar-header -->
-      <nav class="navbar navbar-expand-lg navbar-light" id="header_content">
-        <div class="collapse navbar-collapse" id="navbar-content">
-          <?php
-          wp_nav_menu( array(
-            'theme_location'  => 'header-menu',
-            'depth'	          => 2, // 1 = no dropdowns, 2 = with dropdowns.
-            'container'       => false,
-            'container_id'    => 'bs-example-navbar-collapse-1',
-            'menu_class'      => 'navbar-nav mr-auto',
-            'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-            'walker'          => new WP_Bootstrap_Navwalker(),
-          ) );
-          ?>
-        </div>
-      </nav>
+      <a class="navbar-brand" href="/">
+            <?php
+              $custom_logo_id = get_theme_mod( 'custom_logo' );
+              $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+            ?>
+            <img src="<?php echo $image[0]; ?>" alt="<?php echo get_bloginfo( 'name', 'raw' )?>"">
+      </a><!-- /.navbar-brand -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <?php
+        wp_nav_menu( array(
+          'theme_location'  => 'header-menu',
+          'depth'	          => 2, // 1 = no dropdowns, 2 = with dropdowns.
+          'container'       => false,
+          'container_id'    => 'bs-example-navbar-collapse-1',
+          'menu_class'      => 'navbar-nav mr-auto',
+          'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+          'walker'          => new WP_Bootstrap_Navwalker(),
+        ) );
+        ?>
+      </div>
     </div>
-  </div>
+  </nav>
 </header>
